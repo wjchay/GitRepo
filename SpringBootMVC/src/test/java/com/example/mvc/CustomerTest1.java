f521339c4ad1d4bd8b1b297ec8e93ec8f8bf5e53
wjchay <51632584+wjchay@users.noreply.github.com>
2019-07-20 18:38:48 +0800

    updated

--- SpringBootMVC/src/test/java/com/example/mvc/CustomerTest1.java
+++ SpringBootMVC/src/test/java/com/example/mvc/CustomerTest1.java
@@ -6,25 +6,24 @@ import org.junit.jupiter.api.Test;
 
 class CustomerTest1 {
 
-    
     @Test
     void testHashCode() {
-        //fail("Not yet implemented"); // TODO
+        fail("Not yet implemented"); // TODO
     }
 
     @Test
     void testCustomerStringString() {
-        //fail("Not yet implemented"); // TODO
+        fail("Not yet implemented"); // TODO
     }
 
     @Test
     void testToString() {
-        //fail("Not yet implemented"); // TODO
+        fail("Not yet implemented"); // TODO
     }
 
     @Test
     void testEqualsObject() {
-        //fail("Not yet implemented"); // TODO
+        fail("Not yet implemented"); // TODO
     }
 
 }

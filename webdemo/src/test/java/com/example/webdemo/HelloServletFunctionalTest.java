f1f754553f7759b588270b094dfddb4cbdf9ecd1
wjchay <51632584+wjchay@users.noreply.github.com>
2019-07-20 11:25:54 +0800

    updated

---
+++ webdemo/src/test/java/com/example/webdemo/HelloServletFunctionalTest.java
@@ -0,0 +1,44 @@
+package com.example.webdemo;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HelloServletFunctionalTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+        //ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup(); 
+    }
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();               
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();                         
+    }
+
+    @Test
+    public void sayHello() throws Exception {      
+        driver.get("http://localhost:8080/webdemo");
+
+        driver.findElement(By.id("say-hello-text-input")).sendKeys("Dolly");
+        driver.findElement(By.id("say-hello-button")).click();
+
+        assertEquals("Hello Page", driver.getTitle());
+        assertEquals("Hello, Dolly!", driver.findElement(By.tagName("h2")).getText());
+    }
+}
